7519cc6a5b78fad2626a7f7508b90bc3bb9e21e7
rafaelsvieira89 <38160917+rafaelsvieira89@users.noreply.github.com>
2022-04-30 14:31:41 -0300

    Refactor for className of ProductResourceItTests.java

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceItTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceItTests.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-public class ProductResourceIt {
+public class ProductResourceItTests {
 
     @Autowired
     private MockMvc mockMvc;
