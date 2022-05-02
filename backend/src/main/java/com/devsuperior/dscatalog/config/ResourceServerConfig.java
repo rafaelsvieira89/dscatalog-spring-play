e0fd03f0cc9fdb0a6ae43a577a0bdaf6f87f2a9e
rafaelsvieira89 <38160917+rafaelsvieira89@users.noreply.github.com>
2022-05-01 23:22:08 -0300

    03-27 H2 security confiuration for local environment

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,14 +10,18 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer // torna a classe como um servidor de recursos do OAUTH2
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment environment; //Ambiente de toda a aplicação Spring
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
 
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
 
@@ -30,6 +35,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        if(Arrays.asList(environment.getActiveProfiles()).contains("local")){
+            /*
+            A interface do H2 requer que desabilite os frames
+             */
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
